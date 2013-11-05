9d030ba259d11bd30c54aaa5f5341881ac867635
George Reyes <zypher452@yahoo.com>
2013-11-04 20:25:09 -0800

    Removed vpn files as trafficstats goes in depth enough for my needs in the project. Added in the application metrics and how much data is being used

--- FireRwar/src/com/example/firerwar/metricsViewer.java
+++ FireRwar/src/com/example/firerwar/metricsViewer.java
@@ -1,12 +1,19 @@
 package com.example.firerwar;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.TrafficStats;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -25,7 +32,7 @@ public class metricsViewer extends Fragment{
 	Socket temp;
 
 	Context mContext;
-	VpnClient connectVpn = new VpnClient();
+
 	
     public void setContext (Context mContext) {
     	this.mContext = mContext;
@@ -36,66 +43,81 @@ public class metricsViewer extends Fragment{
     	
         View rootView = inflater.inflate(R.layout.form,container,false);
         
-        rootView.findViewById(R.id.connect)
-        .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), VpnClient.class);
-                startActivity(intent);
-            }
-        });
 
-    	return rootView;//printNetworkSettings(mContext, rootView);
+    	return printNetworkSettings(mContext, rootView);
     }
 
-
+/* You'll want to make a uid hashtable and then update the values
+ * in the listview whenever one of them increases, these increases 
+ * should also be reflected in the graph view that I choose to use */
 public View printNetworkSettings(Context mContext, View rootView) {
 	
 	
 	TextView portDisplay = (TextView) rootView.findViewById(R.id.Namer);
 	LinearLayout tempView = (LinearLayout) rootView.findViewById(R.id.LinearPortHolder);
 	ListView listerPorts = (ListView) rootView.findViewById(R.id.PortItems);
-	//ListView tempView = (ListView) rootView;
+	PackageManager packer = mContext.getPackageManager();
+
 
 	
 	ArrayList<String> ipViewText = new ArrayList<String>();
 	
 	portDisplay.setText("Ports Blocked");
-	//tempView.addView(portDisplay);
-	
-	//tempView.addView(ipAddress);
+
 	
 	ArrayAdapter<String> adapter;
 	adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, ipViewText);
 	
-	
-	//ipAddress.setText(intToIp(addr.ipAddress)+"\n");
-	
-	
-	//tempView = ((LinearLayout) rootView.findViewById(R.id.listItems));
 
 	listerPorts.setAdapter(adapter);
-	//tempView.addView(listerPorts);
-	
-	ipViewText.add("Metrics shown here");
-//	ipViewText.add("IP Address: "+intToIp(addr.ipAddress)+"\n");
-//	ipViewText.add("Subnet Mask: " + intToIp(addr.netmask)+"\n");
-//	ipViewText.add("Default Gateway: " + intToIp(addr.gateway)+"\n");
-//	ipViewText.add("Packets Blocked: " + "<number>" + "\n");
-//	ipViewText.add("IPv4:DNSServers: " + "<?>" + "\n");
-	//TODO: Add button that is named release port so that you can see it working in action
-	//TODO: add text field that shows ports blocked
-	//TODO: look up how to block tcp and udp ports, also how to distinguish between them
-	//TODO: Add network graph to monitor throughput
 	
 	
-	adapter.notifyDataSetChanged();
+	long rxBytes = TrafficStats.getTotalRxBytes();
 
+	long txBytes = TrafficStats.getTotalTxBytes();
 	
-	// ((TextView) rootView.findViewById(android.R.id.text1)).setText(intToIp(addr.ipAddress)+"\n");
-	 
+
+	ipViewText.add("Download: "+Long.toString(rxBytes));
+	ipViewText.add("Uploaded: "+Long.toString(txBytes));
+	
+	
+
+	File dir = new File("/proc/uid_stat/");
+	String[] children = dir.list();
+	List<Integer> uids = new ArrayList<Integer>();
+	if (children != null) {
+	  for (int i = 0; i < children.length; i++) {
+	    int uid = Integer.parseInt(children[i]);
+	    String uidString = String.valueOf(uid);
+	    File uidFileDir = new File("/proc/uid_stat/"+uidString);
+	    File uidActualFile = new File(uidFileDir,"tcp_rcv");
+
+	    try {
+	        BufferedReader br = new BufferedReader(new FileReader(uidActualFile));
+	        String line;
+
+	        while ((line = br.readLine()) != null) {
+	            //Log.d(String.valueOf(uid), line);//this returns the amount of data received for the particular uid
+	        	Log.d(packer.getNameForUid(uid), line);
+	        	ipViewText.add(packer.getNameForUid(uid)+": "+line);
+
+	        }
+	        br.close();
+	        
+	    }
+	    catch (IOException e) {
+	        //handle this
+	    }
+	    
+	      uids.add(uid);
+	    }
+	  
+	  }
+	adapter.notifyDataSetChanged();
 	 return tempView;
 
 }
+
+
 
 }
