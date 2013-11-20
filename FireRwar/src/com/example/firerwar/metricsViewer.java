48d5adc58c67f592fab1ed73a88140652583db6c
George Reyes <zypher452@yahoo.com>
2013-11-19 17:45:31 -0800

    Changed Graphs.xml in order to contain the proper graph layout. Tore out graph view in favor of AChartEngine. Created working realtime updating graph, currently only tracks download with padding of 10 pixels.

--- FireRwar/src/com/example/firerwar/metricsViewer.java
+++ FireRwar/src/com/example/firerwar/metricsViewer.java
@@ -7,15 +7,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.jjoe64.graphview.BarGraphView;
-import com.jjoe64.graphview.CustomLabelFormatter;
-import com.jjoe64.graphview.GraphViewSeries.GraphViewSeriesStyle;
-import com.jjoe64.graphview.LineGraphView;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-import com.jjoe64.graphview.GraphView.LegendAlign;
-import com.jjoe64.graphview.GraphViewSeries;
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 
+import android.app.ActionBar.LayoutParams;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
@@ -28,144 +27,188 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 public class metricsViewer extends FragmentActivity {
-	 private final Handler mHandler = new Handler();
-     private Runnable mTimer1;
-     private Runnable mTimer2;
-     private GraphView graphView;
-     private GraphViewSeries exampleSeries1;
-     private GraphViewSeries exampleSeries2;
-     private double graph2LastXValue = 5d;
-     private GraphViewSeries exampleSeries3;
 
-     
+	private XYMultipleSeriesDataset DownloadDataSet = new XYMultipleSeriesDataset();
+	private XYMultipleSeriesRenderer DownloadRenderer = new XYMultipleSeriesRenderer();
+	private XYSeries DLCurrentSeries; /*
+									 * make this an arrayList of xy series ,
+									 * then loop through that list updating the
+									 * the graph with the repective values
+									 */
+	private GraphicalView DLChartView;
+	private final Handler mHandler = new Handler();
+	private Runnable mTimer1;
+	public static int y;
+	public ArrayList<XYSeries> m_SeriesData;
 
-     private double getRandom() {
-             double high = 3;
-             double low = 0.5;
-             return Math.random() * (high - low) + low;
-     }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.graphs);
+		DownloadRenderer.setAxisTitleTextSize(16);
+		DownloadRenderer.setChartTitleTextSize(20);
+		DownloadRenderer.setLabelsTextSize(15);
+		DownloadRenderer.setLegendTextSize(15);
+		DownloadRenderer.setMargins(new int[] { 20, 30, 15, 0 });
+		DownloadRenderer.setYLabelsPadding(10);
+		DownloadRenderer.setAxesColor(Color.YELLOW);
 
-     /** Called when the activity is first created. */
-     @Override
-     public void onCreate(Bundle savedInstanceState) {
-             super.onCreate(savedInstanceState);
-             setContentView(R.layout.graphs);
-             
+		String seriesTitle = "Download Display";
+		XYSeries series = new XYSeries(seriesTitle);
 
-             // init example series data
-             exampleSeries1 = new GraphViewSeries(new GraphViewData[] {
-                             new GraphViewData(1, 2.0d)
-                             , new GraphViewData(2, 1.5d)
-                             , new GraphViewData(2.5, 3.0d) // another frequency
-                             , new GraphViewData(3, 2.5d)
-                             , new GraphViewData(4, 1.0d)
-                             , new GraphViewData(5, 3.0d)
-             });
-             exampleSeries3 = new GraphViewSeries(new GraphViewData[] {});
-             exampleSeries3.getStyle().color = Color.CYAN;
+		DownloadDataSet.addSeries(series);
+		DLCurrentSeries = series;
+		/**/
+		
 
-             // graph with dynamically genereated horizontal and vertical labels
-             graphView = new LineGraphView(
-                     this // context
-                     , "GraphViewDemo" // heading
-            		 );
-//             if (getIntent().getStringExtra("type").equals("bar")) {
-//                     graphView = new BarGraphView(
-//                                     this // context
-//                                     , "GraphViewDemo" // heading
-//                     );
-//             } else {
-//                     graphView = new LineGraphView(
-//                                     this // context
-//                                     , "GraphViewDemo" // heading
-//                     );
-//             }
-             graphView.addSeries(exampleSeries1); // data
-             graphView.addSeries(exampleSeries3);
+		XYSeriesRenderer renderer = new XYSeriesRenderer();
+		renderer.setColor(Color.RED);
+		DownloadRenderer.addSeriesRenderer(renderer);
+		printNetworkSettings();
 
-             LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);
-             layout.addView(graphView);
+	}
+	
+	/* function that will add series to the graph*/
+	public void addNewSeries () {
+		
+	}
 
-             // ----------
-//             exampleSeries2 = new GraphViewSeries(new GraphViewData[] {
-//                             new GraphViewData(1, 2.0d)
-//                             , new GraphViewData(2, 1.5d)
-//                             , new GraphViewData(2.5, 3.0d) // another frequency
-//                             , new GraphViewData(3, 2.5d)
-//                             , new GraphViewData(4, 1.0d)
-//                             , new GraphViewData(5, 3.0d)
-//             });
+	@Override
+	protected void onPause() {
 
-             // graph with custom labels and drawBackground
-//             if (getIntent().getStringExtra("type").equals("bar")) {
-//                     graphView = new BarGraphView(
-//                                     this
-//                                     , "GraphViewDemo"
-//                     );
-//             } else {
-//                     graphView = new LineGraphView(
-//                                     this
-//                                     , "GraphViewDemo"
-//                     );
-//                     ((LineGraphView) graphView).setDrawBackground(true);
-//             }
-            // graphView.addSeries(exampleSeries2); // data
-            // graphView.setViewPort(1, 8);
-            // graphView.setScalable(true);
+		super.onPause();
+	}
 
-             //layout = (LinearLayout) findViewById(R.id.graph2);
-             //layout.addView(graphView);
-     }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (DLChartView == null) {
+			LinearLayout layout = (LinearLayout) findViewById(R.id.graph);
+			DLChartView = ChartFactory.getLineChartView(this, DownloadDataSet,
+					DownloadRenderer);
 
-     @Override
-     protected void onPause() {
-             mHandler.removeCallbacks(mTimer1);
-             mHandler.removeCallbacks(mTimer2);
-             super.onPause();
-     }
+			layout.addView(DLChartView);
+		} else {
+			DLChartView.repaint();
+		}
 
-     @Override
-     protected void onResume() {
-             super.onResume();
-             final int q = (int) TrafficStats.getTotalRxBytes();
+		mTimer1 = new Runnable() {
+			@Override
+			public void run() {
+				//printNetworkSettings();
+				DLCurrentSeries.add(y, TrafficStats.getTotalRxBytes());
+				y++;
+				if (DLChartView != null) {
+					DLChartView.repaint();
+				}
+				mHandler.postDelayed(this, 2000);
+			}
+		};
+		mHandler.postDelayed(mTimer1, 300);
 
-             mTimer1 = new Runnable() {
-                     @Override
-                     public void run() {
-                    	 final int[] stats = {(int) TrafficStats.getTotalRxBytes(),
-                    	                      (int) TrafficStats.getTotalRxBytes(),
-                    	                      (int) TrafficStats.getTotalRxBytes(),
-                    	                      (int) TrafficStats.getTotalRxBytes(),
-                    	                      (int) TrafficStats.getTotalRxBytes()};
-                    	 
-                    	 exampleSeries1.resetData(new GraphViewData[] {
-                                 new GraphViewData(1,stats[0])
-                                 , new GraphViewData(2, stats[1])
-                                 , new GraphViewData(3, stats[2])
-                                 , new GraphViewData(4, stats[3])
-                                 , new GraphViewData(5, stats[4])
-                    	 });
+	}
 
-                             mHandler.postDelayed(this, 300);
-                     }
-             };
-             mHandler.postDelayed(mTimer1, 300);
+	//
+	// @Override
+	// public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	// Bundle savedInstanceState) {
+	//
+	// View rootView = inflater.inflate(R.layout.form, container, false);
+	//
+	// return printNetworkSettings(mContext, rootView);
+	// }
 
-             mTimer2 = new Runnable() {
-                     @Override
-                     public void run() {
-                             graph2LastXValue += 1d;
-                             exampleSeries2.appendData(new GraphViewData(graph2LastXValue, getRandom()), true, 10);
-                             mHandler.postDelayed(this, 1000);
-                     }
-             };
-             //mHandler.postDelayed(mTimer2, 1000);
-     }
+	/*
+	 * You'll want to make a uid hashtable and then update the values in the
+	 * listview whenever one of them increases, these increases should also be
+	 * reflected in the graph view that I choose to use
+	 */
+	public View printNetworkSettings() {
+
+		TextView portDisplay = (TextView) findViewById(R.id.Namer);
+		LinearLayout tempView = (LinearLayout) findViewById(R.id.LinearPortHolder);
+		ListView listerPorts = (ListView) findViewById(R.id.PortItems);
+
+		ArrayList<String> ipViewText = new ArrayList<String>();
+
+		ArrayAdapter<String> adapter;
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, ipViewText);
+
+		listerPorts.setAdapter(adapter);
+
+		// calculates how much data has been downloaded and uploaded from boot.
+		long rxBytes = TrafficStats.getTotalRxBytes();
+		long txBytes = TrafficStats.getTotalTxBytes();
+		ipViewText.add("Download: " + Long.toString(rxBytes));
+		ipViewText.add("Uploaded: " + Long.toString(txBytes));
+
+		// handles reading in all of the applications name and their data
+		readApplicationPackageNames(ipViewText);
+		listerPorts.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) 
+			{
+				int i = 0;
+				Log.d("Long clicked","hi");
+				//Toast.makeText(this, "You long clicked a thing!",Toast.LENGTH_SHORT).show();
+				return false;
+				
+			}});
+
+		// handles adding the graphview and it's colors/layouts
+
+		adapter.notifyDataSetChanged();
+		return tempView;
+
+	}
+
+	public ArrayList<String> readApplicationPackageNames(
+			ArrayList<String> infoPackage) {
+
+		PackageManager packer = this.getPackageManager();
+		File dir = new File("/proc/uid_stat/");
+		String[] children = dir.list();
+		List<Integer> uids = new ArrayList<Integer>();
+		if (children != null) {
+			for (int i = 0; i < children.length; i++) {
+				int uid = Integer.parseInt(children[i]);
+				String uidString = String.valueOf(uid);
+				File uidFileDir = new File("/proc/uid_stat/" + uidString);
+				File uidActualFile = new File(uidFileDir, "tcp_rcv");
+
+				try {
+					BufferedReader br = new BufferedReader(new FileReader(
+							uidActualFile));
+					String line;
+
+					while ((line = br.readLine()) != null) {
+						infoPackage
+								.add(packer.getNameForUid(uid) + ": " + line);
+					}
+					br.close();
+
+				} catch (IOException e) {
+					// handle this
+				}
+
+				uids.add(uid);
+			}
+
+		}
+		return infoPackage;
+	}
+
 }
